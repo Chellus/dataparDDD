3ec33c9d5fade27e9f740aec317535d39a235419
Chellus <elias4845@gmail.com>
2024-10-30 21:46:18 -0300

    Removed unused constructors

--- src/main/java/com/chellus/TiendaCRUD/Client/ClientDTO.java
+++ src/main/java/com/chellus/TiendaCRUD/Client/ClientDTO.java
@@ -14,28 +14,6 @@ public class ClientDTO {
 
     public ClientDTO() {}
 
-    public ClientDTO(Long id, String name, String address, List<Long> customerOrdersId, String phone) {
-        this.id = id;
-        this.name = name;
-        this.address = address;
-        this.customerOrdersId = customerOrdersId;
-        this.phone = phone;
-    }
-
-    public ClientDTO(Client client) {
-        this.id = client.getId();
-        this.name = client.getName();
-        this.address = client.getAddress();
-        this.phone = client.getPhone();
-        List<Long> customerOrdersId = new ArrayList<>();
-
-        for (CustomerOrder order : client.getOrders()) {
-            customerOrdersId.add(order.getId());
-        }
-
-        this.customerOrdersId = customerOrdersId;
-    }
-
     public Long getId() {
         return id;
     }
